2bb9223269081f75c07a21c4473c8e4fc1c5c6cf
Dima Gaykov <dima.gaykov.04@gmail.com>
2022-10-26 21:20:04 +0300

    lab13 full

---
+++ Сем1/ООП/lab13/Lab13Re/APlant.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace lab4;
+[Serializable]
+public abstract class APlant
+{
+    #region Fields
+    
+    protected TypePlant _type;
+    [NonSerialized]
+    protected string _name = "deflaut";
+    protected DateTime _wasPlanted;
+
+    #endregion
+
+    #region Properties
+
+    public virtual TypePlant Type 
+    { 
+        get => _type;
+        set => _type = value;
+    }
+    [JsonIgnore]
+    [XmlIgnore]
+    public virtual string Name 
+    {
+        get => _name; 
+        set => _name = value;
+    }
+    public virtual DateTime WasPlanted 
+    {
+        get => _wasPlanted;
+        set => _wasPlanted = value;
+    }
+    public abstract DateTime WillBeRipen { get;  }
+
+    #endregion
+
+    public APlant(string Name) =>
+        this.Name = Name;
+
+    public APlant(TypePlant type,
+                  string name,
+                  DateTime wasPlanted)
+    {
+        Type = type;
+        Name = name;
+        WasPlanted = wasPlanted;
+    }
+
+    public APlant() { }
+
+    public abstract void Plant();
+}
+
+public enum TypePlant
+{
+    BUSH,
+    TREE,
+    FLOWER,
+    CACTUS
+}
--- Сем1/ООП/lab13/Lab13Re/Program.cs
+++ Сем1/ООП/lab13/Lab13Re/Program.cs
@@ -4,15 +4,17 @@ global using System.IO;
 global using System.Text.Json;
 global using System.Xml.Serialization;
 global using System.Xml.XPath;
+using System.Xml;
+using System.Text.Json.Nodes;
 
 namespace lab13;
 internal class Program
 {
-   
+
     static void Main(string[] args)
     {
         Tree tree = new("Дуб");
-        
+
         tree.Plant();
 
         /* . Из лабораторной №4 выберите класс с наследованием и/или
@@ -36,6 +38,32 @@ d.XML.*/
             Console.WriteLine(tree2);
             Console.WriteLine(tree3);
 
+            // ex3
+
+            XmlDocument xml = new();
+            xml.Load("Tree.xml");
+
+            var doc = xml.DocumentElement;
+
+            var info = doc.SelectNodes("WasPlanted");
+
+            foreach (XmlNode type in info)
+            {
+                Console.WriteLine("XML: " + type.InnerText);
+            }
+
+            // ex4
+
+            // Используя Linq to XML (или Linq to JSON) создайте новый xml (json) -
+            // документ и напишите несколько запросов
+
+            JsonDocument jsonDocument = JsonDocument.Parse(File.ReadAllText("Tree.json"));
+
+            JsonElement root = jsonDocument.RootElement;
+
+            var json = root.GetProperty("WasPlanted");
+
+            Console.WriteLine("Json: " + json);
         }
         catch (Exception e)
         {
